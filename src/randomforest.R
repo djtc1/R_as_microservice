2f704418400013c8e650c9fc063957804a74372c
Sofiane M'Barki <sofiane.mbarki@smbalgorithm.com>
2018-03-09 18:12:43 +0000

    RESTRUCTURATION

--- src/randomforest.R
+++ src/randomforest.R
@@ -11,23 +11,32 @@
 #* @post /meetup 
 meetup <- function(ntree, na_wtf) {
   
+  #########################################################
+  #
+  ## PREPARATION PART
+  #
+  #########################################################
+  
   # Load packages
   library(randomForest) # Randomforest of course
   library(dplyr) # Dplyr, of course -> (http://dplyr.tidyverse.org/)
   library(rjson)
 
-  
   # Load data from AWS
   load(url("https://s3.eu-west-3.amazonaws.com/meetup.public.data/lycee.RData"))
   
   
+  #########################################################
+  #
+  ## TRAINING PART
+  #
+  #########################################################
+  
   # Split the data {training and testing datasets}
   train <- lycee %>% sample_frac(0.8)
   
-  
   # Set the seed for reproduction the experiment
   set.seed(123)
-  
   
   # Run the random forest ;
   ( model <- randomForest(private_or_public ~ ., 
@@ -35,10 +44,22 @@ meetup <- function(ntree, na_wtf) {
                           na.action = na_wtf))
   
   
+  #########################################################
+  #
+  ## TESTING PART
+  #
+  #########################################################
+  
   # Predict the label from the model
   test <- anti_join(lycee, train)
   (test$predicted <- predict(model, test)) 
   
+  
+  #########################################################
+  #
+  ## RESULTS PART
+  #
+  #########################################################
   
   results <- table(test$predicted, test$private_or_public) %>% toJSON()
 
